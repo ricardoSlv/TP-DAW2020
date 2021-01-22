1fa9ccf7bdc853e173b03a5b457e92f5ccf0614f
LuisDuarte98 <36744365+LuisDuarte98@users.noreply.github.com>
2021-01-22 18:15:36 +0000

    up

---
+++ public/javascripts/post.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('#postForm')
+
+form.addEventListener('submit', async e => {
+
+    e.preventDefault()
+
+    const uploadForm = e.target
+    const { title, subtitle, themes} = uploadForm
+    const [REPORT, THESIS, ARTICLE, APP, SLIDES, TEST, SOLVEDPROB] = [...themes]
+
+    const uploadFormData = new FormData()
+
+    uploadFormData.append(title.name, title.value)
+    uploadFormData.append(subtitle.name, subtitle.value)
+    uploadFormData.append("themes", [...themes].filter(t => t.checked).map(t => t.value))
+    console.table([...uploadFormData.entries()])
+
+    try {
+        const resp = await fetch('/posts/upload', {
+            method: 'POST',
+            body: uploadFormData,
+        })
+        if(resp.status === 200) {
+            alert('Post creation successfull! 😀')
+            window.location.pathname='/posts/upload'
+        }
+        else
+            alert('The server has encountered an error, please retry later 😥')
+    }catch(e){
+        alert('An error has occurred during signup, please retry 😥')
+    }
+
+
+})
